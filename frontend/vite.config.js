b19ebe35cea8b9f3e7f68680c1b44de977940c16
hussainzaidi99 <zaidihussain009@gmail.com>
2026-01-21 10:20:10 +0500

    final commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,19 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('plotly.js')) {
+            return 'plotly';
+          }
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
 })
